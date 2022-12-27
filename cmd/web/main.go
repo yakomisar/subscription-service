19e7c81cb47bd4340f2d6a7a5be6e057e4954de1
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2022-12-27 22:37:41 +0300

    init commit

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+// listening to port 80
+const webPort = 80
+
+func main() {
+	// Connect to the database
+
+	// Create channels
+
+	// Create Waitgroup
+
+	// Setup the application config
+
+	// Setup mail
+
+	// Listen for web connections
+
+}
